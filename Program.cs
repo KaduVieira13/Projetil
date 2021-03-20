e24925a53ee68c7ebbb4488533cb5433e3cbeed8
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-19 21:15:17 -0300

    Lançamento de Projétil

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Projetil
+{
+              class Program
+              {
+                            static void Main(string[] args)
+                            {
+                                          const double g = 9.80665;
+
+                                          double anguloEmGraus;
+                                          double velocidade;
+                                          double anguloEmRadianos;
+                                          double alcance;
+                                          double altura;
+
+                                          Console.WriteLine("--- Projetil---");
+
+                                          Console.Write("entre com a velocidade, em m/s..:");
+                                          velocidade = Convert.ToDouble(Console.ReadLine());
+
+                                          Console.Write("entre com o angulo, em graus..:");
+                                          anguloEmGraus = Convert.ToDouble(Console.ReadLine());
+
+                                          anguloEmRadianos = anguloEmGraus * (Math.PI / 180);
+
+                                          alcance = Math.Pow(velocidade, 2) * Math.Sin(2 * anguloEmRadianos) / g;
+                                          Console.WriteLine($"Alcance é igual: {alcance:N2} m ");
+
+                                          altura = Math.Pow((velocidade * Math.Sin(anguloEmRadianos)), 2) / (2 * g);
+                                          Console.WriteLine($"Altura é igual: {altura:N2}");
+
+
+
+                            }
+              }
+}
